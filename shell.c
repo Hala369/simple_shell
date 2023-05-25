a9a31ef2018b93c9786f6893412fc54780fb00f9
Hala369 <halamastaki@gmail.com>
2023-05-25 06:32:19 -0700

    shell2

--- shell.c
+++ shell.c
@@ -1,110 +1,31 @@
-#include "main.h"
-/**
- *
- * UNIX command line interpreter.
- *
- *
- **/
-int main(void)
-{
-	char input[BUFFER_SIZE];
-	char prompt[] = "ALX: ";
-
-	while (1)
-	{
-	printf("%s", prompt);
-
-	if (fgets(input, BUFFER_SIZE, stdin) == NULL)
-	{
-		printf("\n");
-		break;
-	}
-	input[strcspn(input, "\n")] = '\0';
-	if (strlen(input) == 0)
-	{
-		continue;
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-#define BUFFER_SIZE 1024
-#define TOKEN_DELIMITERS " \t\n\r\a"
 
 int main(void)
 {
-	char input[BUFFER_SIZE], char prompt[] = "ALX: ";
+	pid_t child_pid;
 
-	while (1)
+	child_pid = fork();
+	if (child_pid == -1)
 	{
-	printf("%s", prompt);
-	// Display the prompt
-	if (fgets(input, BUFFER_SIZE, stdin) == NULL)
+		perror("fork error");
+		exit(1);
+	}
+	else if (child_pid == 0)
 	{
-		printf("\n");
-		// Print a newline if Ctrl+D is pressed
-		break;
-		// Exit the shell on end of file
+		char *args[] = {"/bin/ls", "-l", NULL};
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror("execve error");
+			_exit(1);
 	}
-	input[strcspn(input, "\n")] = '\0';
-	// Remove trailing newline
-	if (strlen(input) == 0)
+}
+else
 	{
-		continue;
-		// Ignore empty commands and display the prompt again
->>>>>>> 6244625d9a57e77c8f9c26e2751382d32967b53d
+		wait(NULL);
+		write(STDOUT_FILENO, "Child process executed.\n", 24);
 	}
-	if (strcmp(input, "exit") == 0)
-	{
-		break;
-<<<<<<< HEAD
-	}
-	char *token = strtok(input, TOKEN_DELIMITERS), *command = token, *arguments[BUFFER_SIZE];
-=======
-		// Exit the shell
-	}
-	char *token = strtok(input, TOKEN_DELIMITERS);
-	char *command = token, *arguments[BUFFER_SIZE];
->>>>>>> 6244625d9a57e77c8f9c26e2751382d32967b53d
-	int arg_index = 0;
 
-	while (token != NULL)
-	{
-		arguments[arg_index] = token;
-		arg_index++;
-		token = strtok(NULL, TOKEN_DELIMITERS);
-	}
-	arguments[arg_index] = NULL;
-	// Set the last element to NULL
-	pid_t pid = fork();
-
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-		}
-	if (pid == 0)
-	{
-	if (execvp(command, arguments) == -1)
-	{
-	perror("exec");
-	exit(EXIT_FAILURE);
-<<<<<<< HEAD
-		}
-	} else
-	{
-	wait(NULL); // Wait for the child process to complete
-=======
-	}
-	}
-	else
-	{
-	wait(NULL);
-	// Wait for the child process to complete
->>>>>>> 6244625d9a57e77c8f9c26e2751382d32967b53d
-	}
-	}
-	printf("thallaw...\n");
 	return (0);
 }
